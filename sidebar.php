d7aee604d395cf164f248d3cc8872d87e57b8176
Kyle McCaf <kylemccaf@outlook.com>
2018-01-02 00:33:01 -0800

    Featured Images for featured news

--- sidebar.php
+++ sidebar.php
@@ -13,6 +13,34 @@
 			 while ( $the_query->have_posts() && $myCount <= 3) : $the_query->the_post();
 			 $myCount = $myCount + 1;
 			  ?>
+			  
+			  <div class="col-xs-12 col-sm-4">
+				<div class="panel post">
+				  <?php
+					if ( has_post_thumbnail() ) { // check if the post has a featured image assigned to it.
+						echo "<a class=\"post-link\" href=\"";
+						the_permalink();
+						echo "\"><img src=\"";
+						the_post_thumbnail_url( 'medium' ); // 300px x 300px resolution
+						echo "\" srcset=\"";
+						the_post_thumbnail_url( 'medium' ); // 300px x 300px medium resolution
+						echo " 300w, ";
+						the_post_thumbnail_url( 'large' ); // 640px x 640px medium resolution
+						echo " 640w, ";
+						the_post_thumbnail_url( 'full' ); // 1024px x 1024px medium resolution
+						echo " 1024w\" width=\"100%\"></a>";
+					}
+				  ?>
+				  <div class="panel-body">
+					<center>
+					<h2><a class="post-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<?php the_content(__('<div class="read-more"><p class="btn btn-block btn-default">Read More</p></div>')); ?>
+					</center>
+				  </div>
+				</div>
+			  </div>
+			  
+			  <!--
 				<div class="col-xs-12 col-sm-4">
 				  <div class="well">
 				    <center>
@@ -21,7 +49,7 @@
 					</center>
 				  </div>
 				</div>
-				
+				-->
 		<?php endwhile;?>
       </div>
     </div>
