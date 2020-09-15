c678bc3d42b048789c861d09bdbe172f996ac0e4
Kyle McCaf <kylemccaf@outlook.com>
2020-09-14 22:29:53 -0700

    Cleaner navbar menu

--- functions/navbars.php
+++ functions/navbars.php
@@ -4,61 +4,54 @@
  */
 
 // Register the menu locations
-if ( function_exists( 'register_nav_menus' ) ) {
+add_action('after_setup_theme', function(){
 	register_nav_menus(
 		array(
 		  'nav_menu' => 'Nav Menu',
 		  'footer_menu' => 'Footer Menu'
 		)
 	);
-}
+});
 
-function get_header_menu($menu, $menu_items){
-	$count = 0;
+function get_header_menu($menu_items){
+	$menu_list = '<ul id="menu-header" class="navbar-nav ml-auto">';
 	$submenu = false;
-	$menu_list = "<ul id=\"menu-header\" class=\"navbar-nav ml-auto\">\n";
 	
-	foreach( (array)$menu_items as $menu_item ) {
-		 
-		$link = $menu_item->url;
-		$title = $menu_item->title;
-		
-		// If the menu_item has a sub-menu
-		if ( isset($menu_items[ $count + 1 ]) && $menu_item->ID == $menu_items[ $count + 1 ]->menu_item_parent) {
-			if ( !$submenu ) {
-				$submenu = true;
-				$menu_list .= "\t<li class=\"dropdown\">\n";
-				$menu_list .= "\t\t<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">$title <span class=\"caret\"></span></a>\n";
-				$menu_list .= "\t\t\t<ul class=\"dropdown-menu\">\n";
-				$menu_list .= "\t\t\t".'<li class="item"><a href="'.$link.'" class="title">'.$title.'</a></li>' ."\n";
-			}
-		}
-		
-		// If this is the last sub-menu item
-		else if ( isset($menu_items[ $count + 1 ]) && $menu_item->menu_item_parent != $menu_items[ $count + 1 ]->menu_item_parent && $submenu ){
-			$menu_list .= "\t\t\t".'<li class="item"><a href="'.$link.'" class="title">'.$title.'</a></li>' ."\n";
-			$menu_list .= "\t\t".'</ul>'."\n\t".'</li>'."\n";
+	foreach($menu_items as $key => $menu_item) {
+
+		// Clear out dropdown menu if menu_item is root
+		if( $submenu && $menu_item->menu_item_parent == 0 ){
 			$submenu = false;
+			$menu_list .= '</ul></li>';
 		}
-		
-		// Otherwise a regular menu item
-		else {
-			$menu_list .= "\t\t\t<li><a href=\"$link\">$title</a></li>\n";
+
+		// If the menu_item is root and has a child
+		if( !$submenu 
+			&& $menu_item->menu_item_parent == 0
+			&& isset($menu_items[$key+1])
+			&& $menu_item->ID == $menu_items[$key+1]->menu_item_parent ) {
+
+			$submenu = true;
+			$menu_list .= '
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+						'.$menu_item->title.'
+					</a>
+					<ul class="dropdown-menu">'."\n";
 		}
-		$count++;
+		$menu_list .= '<li><a href="'.$menu_item->url.'" title="'.$menu_item->title.'">'.$menu_item->title.'</a></li>';
 	}
-	$menu_list .= "\t\t</ul>\n";
+	$menu_list .= "</ul>";
 	return $menu_list;
 }
 
 
-function get_footer_menu($menu, $menu_items){
-	$count = 0;
+function get_footer_menu($menu_items){
 	$submenu = false;
 	$menu_list = '';
 	$parent_id = '';
 
-	foreach( (array)$menu_items as $menu_item ) {
+	foreach($menu_items as $key => $menu_item) {
 		 
 		$link = $menu_item->url;
 		$title = $menu_item->title;
@@ -79,7 +72,7 @@ function get_footer_menu($menu, $menu_items){
 			}
 			$menu_list .= "\t\t\t".'<li class="item"><a href="'.$link.'" class="title">'.$title.'</a></li>' ."\n";
 			// Check if it is the last submenu item
-			if ( $menu_items[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ){
+			if ( $menu_items[$key]->menu_item_parent != $parent_id && $submenu ){
 				$menu_list .= "\t\t".'</ul>'."\n\t".'</nav>'."\n";
 				$submenu = false;
 			}
@@ -91,24 +84,24 @@ function get_footer_menu($menu, $menu_items){
 		}
 		
 		// This is the last menu_item of the current series
-		if ( isset($menu_items[ $count + 1 ]->menu_item_parent) && $menu_items[ $count + 1 ]->menu_item_parent != $parent_id ) { 
+		if ( isset($menu_items[$key]->menu_item_parent) && $menu_items[$key]->menu_item_parent != $parent_id ) { 
 			$menu_list .= '</div>' ."\n";      
 			$submenu = false;
 		}
-		$count++;
 	}
+	return $menu_list;
 }
 
 function clean_custom_menu( $theme_location ) {
 	if ( ($theme_location) && ($locations = get_nav_menu_locations()) && isset($locations[$theme_location]) ) {
-		$menu = get_term( $locations[$theme_location], 'nav_menu' );
+		$menu = get_term($locations[$theme_location]);
 		$menu_items = wp_get_nav_menu_items($menu->term_id);
 
 		if($theme_location=='nav_menu'){
-			$menu_list = get_header_menu($menu, $menu_items);
+			$menu_list = get_header_menu($menu_items);
 		}
 		else if($theme_location=='footer_menu'){
-			$menu_list = get_footer_menu($menu, $menu_items);
+			$menu_list = get_footer_menu($menu_items);
 		}
 	} else {
 		$menu_list = "<!-- no menu defined for $theme_location -->";
