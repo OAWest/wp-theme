80789821567d97e665806ec89ee6c184eb8b12f6
Kyle McCaf <kylemccaf@outlook.com>
2020-09-16 22:14:38 -0700

    Fixed update dashboard item

--- functions/admin.php
+++ functions/admin.php
@@ -20,20 +20,30 @@ add_action('wp_dashboard_setup', function() {
 		$git_file = wp_remote_get('https://api.github.com/repos/oawest/wp-theme/releases/latest');
 		$git_file = wp_remote_retrieve_body($git_file);
 		$git_data = json_decode($git_file);
-		$latest_version	= $git_data['tag_name'] ?? '';
+		$latest_version	= $git_data->tag_name ?? '';
 
-		$message = ($installed_version==$latest_version || $installed_version=='WR_Child')
-			? '<p>You are on the latest version</p>'
-			: '<h2 style="background-color:rgb(128, 0, 0); color:white; padding:15px;">
+		$message = '';
+		if(empty($latest_version)){
+			$message = '<p>Checking for updates failed. Please visit our <a href="https://github.com/oawest/wp-theme/releases/latest" target="_blank">GitHub</a> to manually check for updates.</p>';
+		}
+		else if($installed_version == $latest_version || $installed_version=='WR_Child'){
+			$message = '<p>You are on the <a href="https://github.com/oawest/wp-theme/releases/latest" target="_blank" title="'.$latest_version.'">latest version</a></p>';
+		}
+		else if($installed_version >= $latest_version){
+			$message = '<p>You are on a development version. Checkout GitHub to downgrade to the <a href="https://github.com/oawest/wp-theme/releases/latest" target="_blank" title="'.$latest_version.'">latest version stable</a></p>';
+		}
+		else{
+			$message = '<h2 style="background-color:rgb(128, 0, 0); color:white; padding:15px;">
 					An update is available for your theme.
 					<a href="https://github.com/oawest/wp-theme/releases/latest" target="_blank" style="color: gold;text-decoration: none;">
 						Version '.$latest_version.'
 					</a>
 				</h2>';
+		}
 
 		echo '<p>Welcome to the Western Region WordPress Theme</p>
 			<p>Need help? Contact the development team at:<br/><a href="mailto:technology@western.oa-bsa.org">technology@western.oa-bsa.org</a></p>
-			<div style="width:100%; text-align:center;"><img src="'.get_template_directory_uri().'/images/logos/logo.jpg" style="max-width:100%; width:150px;"></div>
+			<div style="width:100%; text-align:center;"><img src="'.get_template_directory_uri().'/assets/images/logos/logo.jpg" style="max-width:100%; width:150px;"></div>
 			<p>Installed theme version: <i>'.$installed_version.'</i></p>
 			'.$message;
 	});
